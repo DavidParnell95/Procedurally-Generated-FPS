aaccf226a7edd8bbf19e37368db899411842a791
Davidparnell <c15704085@mydit.ie>
2019-10-13 23:04:24 +0100

    Initial Commit
    Basic Procedural generation implemented, reset button implemented

---
+++ Procedural Generated FPS/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            RestartGame();
+        }
+    }
+
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+        //mazeInstance.Generate();
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();//Stop all currently running coroutines
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
