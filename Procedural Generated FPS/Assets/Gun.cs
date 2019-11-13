c4b62d0b44f93a9feb2cb74a554125b8048cfd36
Davidparnell <c15704085@mydit.ie>
2019-11-13 16:32:59 +0000

    Player movement and shooting Created

---
+++ Procedural Generated FPS/Assets/Gun.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float Damage = 10f;
+    public float Range = 100f;
+
+    public Camera cam;
+    public ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire 1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        //Check if raycast from current look direction hits 
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            muzzleFlash.Play();
+
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Enemy>();
+
+            //If a valid target found, deal damage
+            if(target != null)
+            {
+                target.TakeDamage(Damage);
+            }
+        }
+    }
+}
---
+++ Procedural Generated FPS/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Verticle");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
