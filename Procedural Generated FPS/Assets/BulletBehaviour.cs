6d8420e470a40e2e024dd0b102157d9ac99c4414
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2019-12-17 17:31:06 +0000

    Projectile Weapons

---
+++ Procedural Generated FPS/Assets/BulletBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float damage = 30f;
+    public GameObject impactEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("hit: " + other.name);
+        Destroy(gameObject);
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if(Physics.Raycast(ray, out hit, 100f))
+        {
+            EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
+          
+            enemy.TakeDamage(damage);
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, .5f);
+        }
+
+    }
+}
---
+++ Procedural Generated FPS/Assets/ProjectileWeapon.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ProjectileWeapon : MonoBehaviour
+{
+    
+    public GameObject bulletPref;
+    public Transform bulletSpawn;
+
+    public float speed = 50;
+    public float lifeTime = 2;
+
+    public float fireRate = 15f;//Fire rate
+
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nexTimeToFire = 0f;//When you can shoot next 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nexTimeToFire)
+        {
+            nexTimeToFire = Time.time + 1f / fireRate;
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        muzzleFlash.Play();
+
+        GameObject bullet = Instantiate(bulletPref);
+
+        //Ignore collision with weapon
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(),
+            bulletSpawn.parent.GetComponent<Collider>()
+            );
+
+        bullet.transform.position = bulletSpawn.position;
+
+        Vector3 rotation = bullet.transform.rotation.eulerAngles;
+
+        bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+
+        //only apply one force to the bullet 
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * speed, ForceMode.Impulse);
+
+        //Destroy bullet after time 
+        StartCoroutine(DestroyBullet(bullet, lifeTime));
+    }
+
+    private IEnumerator DestroyBullet(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Destroy(bullet);
+    }
+
+}
+
